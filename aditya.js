5b1cb675853d7e74b2d6a5c1a45349332309fd39
harsha <gunturharsha13@gmail.com>
2023-10-21 12:14:26 +0530

    fresh repo

--- aditya.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-console.log("nenu kojaa ni..");
-=======
-console.log("aditya");
->>>>>>> ae56672c8e9d38e8c17c73ae2066c623a073075d
--- harsha.js
+++
@@ -1,7 +0,0 @@
-
-console.log("hasha vardhan");
-
-console.log("aditya");
-
-console.log("jass");
->>>>>>> ae56672c8e9d38e8c17c73ae2066c623a073075d
