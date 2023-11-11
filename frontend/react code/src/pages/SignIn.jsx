a5709ebcb704ab74253b8e991f9db5ff0ca603e2
Dhilleswara <dhilleswararao19@gmail.com>
2023-11-11 17:33:18 +0530

    sign in html

--- frontend/react code/src/pages/SignIn.jsx
+++ frontend/react code/src/pages/SignIn.jsx
@@ -1,13 +1,29 @@
+//import '../css/SignIn.css'
 export const SignIn = () => {
  
- 
- 
-    return (
+  return (
+    <>
 
-      <>
-      <html></html>
-      </>
-    )
+      <h1>LogIn</h1>
+      <p>Enter your details to get started</p>
 
-  
+      <div class="input1">
+        <h3> Email* </h3>
+        <input autocomplete = 'off' autofocus placeholder='Email' type="text"/>
+      </div>
+
+      <div class="input2">
+        <h3> Password* </h3>
+        <input autocomplete = 'off' placeholder='Password'  type="password"/>
+      </div>
+
+      <button type='search'>
+        Login
+      </button>
+
+      <p>By continuing, you are agreeing to the <link>Terms and Conditions</link> and <link>Privacy Policy</link>.</p>
+
+    </>
+  )
+
 }
