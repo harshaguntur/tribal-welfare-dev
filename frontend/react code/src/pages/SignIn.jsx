2a218ce0faf2c18d061c6b7f000571970d79dfe3
Dhilleswara <dhilleswararao19@gmail.com>
2023-11-11 19:07:44 +0530

    sign in page

--- frontend/react code/src/pages/SignIn.jsx
+++ frontend/react code/src/pages/SignIn.jsx
@@ -1,27 +1,25 @@
-//import '../css/SignIn.css'
+import '../css/SignIn.css'
 export const SignIn = () => {
  
   return (
     <>
 
-      <h1>LogIn</h1>
-      <p>Enter your details to get started</p>
+    <h1>SignIn</h1>
+    <p id="start">Enter your details to get started</p>
 
-      <div class="input1">
-        <h3> Email* </h3>
-        <input autocomplete = 'off' autofocus placeholder='Email' type="text"/>
-      </div>
+    <div className="email">
+      <h3> Email<form>*</form></h3>
+      <input autoFocus placeholder="Email" type="text"/>
+    </div>
+        
+    <div className="password">
+        <h3> Password<form>*</form></h3>
+        <input placeholder="Password"  type="password"/>
+    </div>
 
-      <div class="input2">
-        <h3> Password* </h3>
-        <input autocomplete = 'off' placeholder='Password'  type="password"/>
-      </div>
+    <button type="search"> Signin </button>
 
-      <button type='search'>
-        Login
-      </button>
-
-      <p>By continuing, you are agreeing to the <link>Terms and Conditions</link> and <link>Privacy Policy</link>.</p>
+    <p id="end">By continuing, you are agreeing to the <br/><a href="#">Terms and Conditions</a> and <a href="#">Privacy Policy</a>.</p>
 
     </>
   )
