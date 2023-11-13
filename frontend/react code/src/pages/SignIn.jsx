2fcac070cda707dc72537b059b8788e7fae404f4
harsha <gunturharsha13@gmail.com>
2023-11-13 16:15:42 +0530

    hi

--- frontend/react code/src/pages/SignIn.jsx
+++ frontend/react code/src/pages/SignIn.jsx
@@ -1,6 +1,12 @@
 import '../css/SignIn.css'
+import { Link, NavLink ,useNavigate} from "react-router-dom";
+
+
+
 export const SignIn = () => {
  
+  const navigate = useNavigate();
+
   return (
     <>
      
@@ -18,13 +24,18 @@ export const SignIn = () => {
 
            <input type="submit" value="Signin" className="input btn"/> 
            
+           <div id="SignIn-div">
+              <p>Don't have an account ?</p>
+              <input type="button" value="SignUp" className="input btn"  onClick={()=> navigate("/signup")}/>
+            </div>
+
         </form>
 
            <div className="para">
             <p>
               By continuing you are agreeing to the <br/>
               <span>
-                <a href="#">Terms and conditions</a>
+                <a href="#">Terms and conditions </a>
               </span>
               and
               <span>
