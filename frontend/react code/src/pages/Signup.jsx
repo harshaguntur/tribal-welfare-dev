353aa6f81c2158ea6c5c1e0e584310590673268a
harsha <gunturharsha13@gmail.com>
2023-11-13 11:36:39 +0530

    conneted to backend (signup page)

--- frontend/react code/src/pages/Signup.jsx
+++ frontend/react code/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import '../css/SignUp.css'
+import "../css/SignUp.css";
 
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
@@ -33,91 +33,48 @@ const Signup = () => {
       password,
     });
 
-    let res ;
+    let res;
 
-    axios.get("/api/Signup").then((Response) => res = Response);
+    axios.get("/api/Signup").then((Response) => (res = Response));
     console.log(res);
-
   };
-
-
 
   return (
     <>
-      {/* <div className="signup-box">
-      <h1>SignUp</h1>
-      <form>
-        <label>Username*</label>
-        <input type="useranme" placeholder="" />
-        <label>Email*</label>
-        <input type="email" placeholder="" />
-        <label>Password*</label>
-        <input type="password" placeholder="" />
-        <input type="button" value="Signup" />
-      </form>
+     
 
+      <div className="outer-div">
+        <img src="../../public/images/logo bgremove.png" alt="" id="logo" />
 
-      <div id="SignIn-div">
-        <p>Already have an Account ?</p>
-        <button id="signin-btn">SignIn</button>
+        <div className="container">
+          <form onSubmit={handelFormSubmit}>
+            <label>Username*</label> <br />
+            <input type="useranme" placeholder="" className="input" onChange={handelUsername}/> <br />
+            <label>Email*</label> <br />
+            <input type="email" placeholder="" className="input" onChange={handelUsermail} /> <br />
+            <label>Password*</label> <br />
+            <input type="password" placeholder="" className="input" onChange={handelUserpassword}/> <br />
+            <input type="submit" value="Signup" className="input btn" /> <br />
+            <div id="SignIn-div">
+              <p>Already have an Account ?</p>
+              <input type="button" value="SignIn" className="input btn" />
+            </div>
+          </form>
+
+          <div className="para">
+            <p>
+              By continuing you are agreeing to the <br />
+              <span>
+                <a href="#">Terms and conditions</a>
+              </span>{" "}
+              and
+              <span>
+                <a href="#"> Privacy Policy </a>
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
-
-
-
-
-      <div className="para-2">
-
-        <p>
-          By continuing you are agreeing to the <br />
-          <a href="#">Terms and conditions</a>and
-          <a href="#">Privacy Policy </a>
-        </p>
-      </div>
-    </div> */}
-
-
-    <div className="outer-div">
-
-
-
-      <img src="../../public/images/logo bgremove.png" alt="" id="logo" />
-
-      <div className="container">
-      <form>
-        <label>Username*</label>                      <br />
-        <input type="useranme" placeholder="" className="input" />      <br />
-        <label>Email*</label>                         <br />
-        <input type="email" placeholder="" className="input"/>         <br />
-        <label>Password*</label>                      <br />
-        <input type="password" placeholder="" className="input"/>      <br />
-        <input type="button" value="Signup" className="input btn"/>        <br />
-
-        <div id="SignIn-div">
-        <p>Already have an Account ?</p>
-        <input type="button" value="SignIn" className="input btn"/> 
-      </div>
-
-
-      </form>
-
-      <div className="para">
-
-        <p>
-          By continuing you are agreeing to the <br />
-          <span><a href="#">Terms and conditions</a></span> and 
-          <span><a href="#"> Privacy Policy </a></span>
-        </p>
-      </div>
-
-
-      </div>
-
-    </div>
-
-
-
-
-
     </>
   );
 };
