0bb35573ed016b6018201f2400361e5a3d6a57ac
harsha <gunturharsha13@gmail.com>
2023-11-13 11:24:41 +0530

    new sign up interface

--- frontend/react code/src/pages/SignIn.jsx
+++ frontend/react code/src/pages/SignIn.jsx
@@ -3,24 +3,30 @@ export const SignIn = () => {
  
   return (
     <>
+      <div className='outer-div'>
 
     <h1>SignIn</h1>
     <p id='start'>Enter your details to get started</p>
 
+    <form action="" id='signIn-form'>
+
     <div className="email">
-      <h3> Email<form>*</form></h3>
+      <h3> Email<span className='star'>*</span></h3>
       <input autoFocus placeholder="Email" type="text"/>
     </div>
         
     <div className="password">
-        <h3> Password<form>*</form></h3>
+        <h3> Password<span className='star'>*</span></h3>
         <input placeholder="Password"  type="password"/>
     </div>
 
-    <button type="search"> Signin </button>
+    <button id='SignIn-btn'> Signin </button>
+
+    </form>
 
     <p id='end'>By continuing, you are agreeing to the <br/><a href='#'>Terms and Conditions</a> and <a href='#'>Privacy Policy</a>.</p>
   
+      </div>
     </>
   )
 
--- frontend/react code/src/pages/Signup.jsx
+++ frontend/react code/src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import '../css/SignUp.css'
 
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
@@ -43,12 +44,81 @@ const Signup = () => {
 
   return (
     <>
-      <p>
+      {/* <div className="signup-box">
+      <h1>SignUp</h1>
+      <form>
+        <label>Username*</label>
+        <input type="useranme" placeholder="" />
+        <label>Email*</label>
+        <input type="email" placeholder="" />
+        <label>Password*</label>
+        <input type="password" placeholder="" />
+        <input type="button" value="Signup" />
+      </form>
 
-        This is the best ting in  the world 
 
-      </p> 
-   </>
+      <div id="SignIn-div">
+        <p>Already have an Account ?</p>
+        <button id="signin-btn">SignIn</button>
+      </div>
+
+
+
+
+      <div className="para-2">
+
+        <p>
+          By continuing you are agreeing to the <br />
+          <a href="#">Terms and conditions</a>and
+          <a href="#">Privacy Policy </a>
+        </p>
+      </div>
+    </div> */}
+
+
+    <div className="outer-div">
+
+
+
+      <img src="../../public/images/logo bgremove.png" alt="" id="logo" />
+
+      <div className="container">
+      <form>
+        <label>Username*</label>                      <br />
+        <input type="useranme" placeholder="" className="input" />      <br />
+        <label>Email*</label>                         <br />
+        <input type="email" placeholder="" className="input"/>         <br />
+        <label>Password*</label>                      <br />
+        <input type="password" placeholder="" className="input"/>      <br />
+        <input type="button" value="Signup" className="input btn"/>        <br />
+
+        <div id="SignIn-div">
+        <p>Already have an Account ?</p>
+        <input type="button" value="Signup" className="input btn"/> 
+      </div>
+
+
+      </form>
+
+      <div className="para">
+
+        <p>
+          By continuing you are agreeing to the <br />
+          <span><a href="#">Terms and conditions</a></span> and 
+          <span><a href="#"> Privacy Policy </a></span>
+        </p>
+      </div>
+
+
+      </div>
+
+    </div>
+
+
+
+
+
+    </>
   );
 };
 export default Signup;
