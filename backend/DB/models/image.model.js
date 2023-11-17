07d4c0a29371bbcf68a391409d18e5d9dbd3e87f
harsha <gunturharsha13@gmail.com>
2023-11-17 12:54:46 +0530

    image backend

---
+++ backend/DB/models/image.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const ImageScehma = new mongoose.Schema(
+    {
+   image : String 
+    },
+
+    {
+        collation : "ImageDetails",
+    }
+
+);
+
+
+const Image = mongoose.model("ImageDetails",ImageScehma);
+
+export default Image; 
--- frontend/react code/src/App.jsx
+++ frontend/react code/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home1 from './pages/Home1'
 import Signup from './pages/Signup'
 import SignIn from './pages/SignIn'
+import PhotoUpload from './pages/PhotoUpload'
 
 
 
@@ -13,7 +14,7 @@ const App = () => {
         <Route path='/signup' element={<Signup/>}/>
         <Route path = '/signin' element={<SignIn/>}/>
         <Route path='/' element={<Home1/>}/>
-     
+        <Route path='/photoupload' element={<PhotoUpload/>} />
          
                   
       </Routes>
