fa9b1692d63f02443df9483124013eb826736db7
harsha <gunturharsha13@gmail.com>
2023-11-19 07:57:25 +0530

    underGoingprojects backend complete

--- frontend/react code/src/pages/UnderGoingProjects.jsx
+++ frontend/react code/src/pages/UnderGoingProjects.jsx
@@ -1,40 +1,41 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
 
-function UnderGoingProjects()  {
-  
-  const [allImages,setAllImages] = useState();
+const UnderGoingProjects = () => {
+ 
+  const [images,setImages] = useState([]);
 
-  const getImages =  async ()=>{
+  const getImages = async ()=>{
     const res = await axios.get("/api/GetUnderGoingProjects");
-    console.log(res);
-    setAllImages(res.data.data);
-    console.log(allImages);
-  }
-  
-  const path = "http://localhost:3000/UnderGoingProjects/";    
-  
-  useEffect(()=>{
-    getImages();
-  },[])         
 
-  
-   
-  
-  
-  
-  
+    const data = res.data.data;
+    setImages(data);
+  }
+
+  const path = "http://localhost:3000/UnderGoingProjects/";
+
+  useEffect(()=>{
+
+    getImages();
+
+  },[])
+
+
+ 
   return (
 
-    <div>
-    
-   
-    <h1>hello world </h1>
-    
-    
-    </div>
-    
 
+    <>
+
+    <div>UnderGoingProjects</div>
+
+    {
+      images.map((ele)=>{
+        return <img key={ele._id} src={path+ele.image} alt="harsha" style={{width:"200px",height:"200px"}} />
+      })
+    }
+
+      </>
   )
 }
 export default UnderGoingProjects
