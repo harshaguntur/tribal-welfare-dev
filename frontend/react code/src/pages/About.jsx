b1f3f6f751b0eafea210b62c72c5fd44c0dd04e1
harsha <gunturharsha13@gmail.com>
2023-11-21 21:12:28 +0530

    about frontend completed

--- frontend/react code/src/pages/About.jsx
+++ frontend/react code/src/pages/About.jsx
@@ -2,29 +2,82 @@ import "../css/About.css"
 
 const About = () => {
   return (
-    <div className="body1">
-        <div className="container1">
-            <div className="about">ABOUT</div>
-            <h1>All about <br/> Vana Ekam</h1>
-        </div>
+    // <div className="body1">
+    //     <div className="container1">
+    //         <div className="about">ABOUT</div>
+    //         <h1>All about <br/> Vana Ekam</h1>
+    //     </div>
     
-        <div className="container2">
-            <p>Vana Ekam is a platform designed to <br/>
-                uplift and empower the underprivileged<br/>
-                tribal communities. Our mission is to work<br/>
-                with various organizations and local<br/>
-                individuals to create sustainable and<br/>
-                meaningful change in the lives of these<br/>
-                communities. We believe that every<br/>
-                individual deserves access to basic<br/>
-                necessities like food, clean water, and<br/>
-                education.</p>   
+    //     <div className="container2">
+    //         <p>Vana Ekam is a platform designed to <br/>
+    //             uplift and empower the underprivileged<br/>
+    //             tribal communities. Our mission is to work<br/>
+    //             with various organizations and local<br/>
+    //             individuals to create sustainable and<br/>
+    //             meaningful change in the lives of these<br/>
+    //             communities. We believe that every<br/>
+    //             individual deserves access to basic<br/>
+    //             necessities like food, clean water, and<br/>
+    //             education.</p>   
             
-            <p>Vana Ekam is a one-of-a-kind website<br/></p>
+    //         <p>Vana Ekam is a one-of-a-kind website<br/></p>
+    //     </div>
+
+    //     <img src="../../react code/public/images/about_.png" alt="logo" className="logo"/>
+    // </div>
+
+
+    <div className="About-outer-div">
+
+
+    <div className="about-container-1">
+
+      <div className="about-container-1-sub-div">
+          <p>ABOUT </p>
+          <h1>All about <br />Vana Ekam</h1>
+      </div>
+
+    </div>
+
+
+
+
+    <div className="about-container-2">
+    <div className="about-container-2-sub-div">
+        
+        <div className="about-container-2-matter-1">
+
+
+        <p>
+        Vana Ekam is a platform designed to <br /> uplift and empower the underprivileged <br /> tribal communities. Our mission is to work <br /> with various organizations and local <br /> individuals to create sustainable and <br />meaningful change in the lives of these <br />communities. We believe that every <br />individual deserves access to basic <br />necessities like food, clean water, and<br /> education.
+        </p>
         </div>
 
-        <img src="../../react code/public/images/about_.png" alt="logo" className="logo"/>
+        <div className="about-container-2-matter-2">
+          <p>
+          Vana Ekam is a one-of-a-kind website <br />that has been developed to promote the <br />welfare of tribal communities in India. We  <br /> aim to establish self-sustaining<br /> communities by providing access to<br /> education, healthcare, and basic<br /> amenities. Our team is dedicated to<br /> working with local stakeholders to<br /> promote sustainable growth and<br /> development in these communities.
+          </p>
+        </div>
+
+
+
+        </div>
     </div>
+
+    <div className="about-container-3">
+    <div className="about-container-3-sub-div">
+        <img src="../../public/tribal photos/Screenshot 2023-11-21 at 9.05.05 PM.png" alt="" />
+        </div>
+    </div>
+
+
+
+
+
+
+    </div>
+
+
   )
 }
 export default About
