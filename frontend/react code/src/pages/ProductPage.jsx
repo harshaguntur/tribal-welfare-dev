af9e43fd59aaf5d47eee0e46575d7d138ed46b86
harsha <gunturharsha13@gmail.com>
2023-11-22 21:56:44 +0530

    product page FE outline completed

--- frontend/react code/src/pages/ProductPage.jsx
+++ frontend/react code/src/pages/ProductPage.jsx
@@ -12,8 +12,11 @@ const ProductPage = () => {
     const data = await axios.post("/api/GetProductById", { id });
     const Newdata = data.data;
     setProduct(Newdata);
+
   };
 
+  // console.log(product);
+  
   const path = "http://localhost:3000/ShopItems/";
 
   useEffect(() => {
@@ -24,15 +27,11 @@ const ProductPage = () => {
     <div className="product-page-outer-div">
       <div className="product-page-left">
         <div className="product-page-left-img-container">
-
-
           <img
             src={path + product?.image}
             alt="product Image"
             id="product-page-product-img"
           />
-
-
         </div>
       </div>
 
@@ -40,9 +39,30 @@ const ProductPage = () => {
 
       <div className="product-page-right">
 
+
+
+        <div className="product-page-right-upper-div">
+          <h1> {product?.ProductName}</h1>
+          <h3>₹{product?.ProductPrice}</h3>
+          <h5>Quantity</h5>
+          <input type="number" name="" id="" />
+          <button>Add to Cart</button>
+          <button>Buy Now</button>
+        </div>
+
+
+
+        <div className="product-page-right-lower-div">
+          <h3>PRODUCT INFO</h3>
+          <p>{product?.ProductDescription}</p>
+        </div>
+
+
+
+
       </div>
 
-      
+
     </div>
   );
 };
