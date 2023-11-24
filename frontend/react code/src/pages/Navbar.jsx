6f5308626daf104a751a622949e9745faf05f821
harsha <gunturharsha13@gmail.com>
2023-11-24 12:02:38 +0530

    nav 2

--- frontend/react code/src/pages/Navbar.jsx
+++ frontend/react code/src/pages/Navbar.jsx
@@ -1,8 +1,33 @@
 import "../css/Navbar.css";
+import {Link,NavLink} from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <div className="navbar-outer-div">
+    
+    <nav>
+   <div className="navbar-left">
+   <img src="../../public/images/logo bgremove.png" id="navbar-logo" alt="" />
+   </div>
+
+
+   <div className="navbar-right">
+
+    <a href="">About</a>
+    <a href="">FAQ</a>
+    <NavLink to="/shop">shop</NavLink>
+    <a href="">Contact Us</a>
+
+
+   </div>
+
+
+
+
+    </nav>
+
+   </div>
   )
 }
-export default Navbar;
+export default Navbar ;   
