58b11ea0c69969f39208f95be39d85ec29e566e2
harsha <gunturharsha13@gmail.com>
2023-11-26 11:04:20 +0530

    harsha now git

--- frontend/react code/src/pages/GetInTouch.jsx
+++ frontend/react code/src/pages/GetInTouch.jsx
@@ -1,54 +1,68 @@
-import "../css/getInTouch.css"
-
+import "../css/getInTouch.css";
 
 const GetInTouch = () => {
   return (
+    // <div className="GetInTouch-outer-div">
+    //   <div className="bgrect">
+    //     <div className="rect1">
 
-    <div className="GetInTouch-outer-div">
+    //       <div className="head">
+    //         <h3>
+    //           {" "}
+    //           Get <br></br> in Touch{" "}
+    //         </h3>
+    //       </div>
 
-      <div className="bgrect">
-        <div className="rect1">
-            <div className="head">
-                <h3> Get <br></br> in Touch </h3> 
-            </div>
 
-            <div className="inputs">
-                
-                <div className="input1">
-                    <h3> First name* </h3>
-                    <input className="inputss" type="text"/>
-                </div>
 
-                <div className="input2">
-                    <h3> Last name* </h3>
-                    <input className="inputss" type="text"/>
-                </div>
+    //       <div className="inputs">
 
-                <div className="input3">
-                    <h3> Email* </h3>
-                    <input className="inputss" type="email"/>
-                </div>
+    //         <div className="input1">
+    //           <h3> First name* </h3>
+    //           <input className="inputss" type="text" />
+    //         </div>
 
-                <div className="input4">
-                    <h3> Phone </h3>
-                    <input className="inputss" type="number"/>
-                </div>
 
-                <div className="input5">
-                    <h3> Company/Organization Name </h3>
-                    <input className="inputss" type="text"/>
-                </div>
+    //         <div className="input2">
+    //           <h3> Last name* </h3>
+    //           <input className="inputss" type="text" />
+    //         </div>
 
-                <div className="help"><a href="#">How can we help? </a></div> 
+    //         <div className="input3">
+    //           <h3> Email* </h3>
+    //           <input className="inputss" type="email" />
+    //         </div>
 
-                <a href="#" className="quote">Get a Quote</a> 
-            </div>
-        </div>
-      </div>
- 
-    </div>
+    //         <div className="input4">
+    //           <h3> Phone </h3>
+    //           <input className="inputss" type="number" />
+    //         </div>
+
+    //         <div className="input5">
+    //           <h3> Company/Organization Name </h3>
+    //           <input className="inputss" type="text" />
+    //         </div>
+
+    //         <div className="help">
+    //           <a href="#">How can we help? </a>
+    //         </div>
+
+    //         <a href="#" className="quote">
+    //           Get a Quote
+    //         </a>
+
+
+    //       </div>
+
+
+    //     </div>
+    //   </div>
+    // </div>
     
+    <div>
+      <h1>hello world</h1>
+    </div>
 
-  )
-}
-export default GetInTouch
+  );
+};
+export default GetInTouch;
--- frontend/react code/src/pages/HomeFinal.jsx
+++ frontend/react code/src/pages/HomeFinal.jsx
@@ -37,7 +37,7 @@ const HomeFinal = () => {
 
     <Donation/>
 
-    <GetInTouch/>
+    
 
 
 
