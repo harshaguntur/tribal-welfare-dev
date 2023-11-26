aa35333124854210d415911ea01ee1ce885b7682
Dhilleswara <dhilleswararao19@gmail.com>
2023-11-26 10:35:14 +0530

    get in touch react

--- frontend/react code/src/pages/GetInTouch.jsx
+++ frontend/react code/src/pages/GetInTouch.jsx
@@ -6,7 +6,45 @@ const GetInTouch = () => {
 
     <div className="GetInTouch-outer-div">
 
-       <h1>Get in Touch div</h1>
+      <div className="bgrect">
+        <div className="rect1">
+            <div className="head">
+                <h3> Get <br></br> in Touch </h3> 
+            </div>
+
+            <div className="inputs">
+                
+                <div className="input1">
+                    <h3> First name* </h3>
+                    <input className="inputss" type="text"/>
+                </div>
+
+                <div className="input2">
+                    <h3> Last name* </h3>
+                    <input className="inputss" type="text"/>
+                </div>
+
+                <div className="input3">
+                    <h3> Email* </h3>
+                    <input className="inputss" type="email"/>
+                </div>
+
+                <div className="input4">
+                    <h3> Phone </h3>
+                    <input className="inputss" type="number"/>
+                </div>
+
+                <div className="input5">
+                    <h3> Company/Organization Name </h3>
+                    <input className="inputss" type="text"/>
+                </div>
+
+                <div className="help"><a href="#">How can we help? </a></div> 
+
+                <a href="#" className="quote">Get a Quote</a> 
+            </div>
+        </div>
+      </div>
  
     </div>
     
