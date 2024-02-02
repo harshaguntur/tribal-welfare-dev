ecb4162ec3405c79f1a0420a34db23747797c44f
harsha <gunturharsha13@gmail.com>
2024-02-02 11:13:13 +0530

    formmated donation modal in backend

--- backend/DB/models/donation.model.js
+++ backend/DB/models/donation.model.js
@@ -1,38 +1,22 @@
 import mongoose from "mongoose";
 
 const DonationProblemsSchema = new mongoose.Schema(
-   {
-
-       image : {
-           type: String,
-           required : true,
-           
-        }, 
-        ProblemName : {
-         type : String,
-         required : true,
-         
-        },
-        ProblemDescription : {
-            type : String,
-            required : true,
-            
-        }
-     
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    ProblemName: {
+      type: String,
+      required: true,
+    },
+    problemdescription: {
+      type: string,
+      required: true,
     }
-
-    
-
-
-
-
-
-
-
+  }
 );
 
-
-const DonationDB  = mongoose.model('donationProblems',DonationProblemsSchema);
-
+const DonationDB = mongoose.model('donationProblems', DonationProblemsSchema);
 
 export default DonationDB; 
